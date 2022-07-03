94ae080b16f4ccd0193d2af3b52f44a3225b2d46
keverza <keverza@protonmail.com>
2022-07-03 22:14:09 +0300

    nutritional val list

--- src/components/InputList.svelte
+++ src/components/InputList.svelte
@@ -12,6 +12,9 @@
 	});
 	$: console.log(ingredientWeight, totalWeight);
 	$: totalWeight = ingredientWeight.reduce((prev, cur) => prev + cur, 0);
+	$: totalCacao = 0;
+
+	$: nutritionalValueList = {};
 	// let curIngr;
 	// let test;
 	// let notcur = editIngr;
@@ -41,11 +44,89 @@
 		// 	console.log(test, 'not: ', notcur, editIngr);
 	}
 
+	function calcIngredientFraction(el, formProps, element) {
+		let setCoef = Math.round((parseInt(formProps[element]) / totalWeight) * 1000) / 1000;
+		return {
+			name: el.name,
+			id: el.id,
+			frfat: el.fat * setCoef,
+			frfatSat: el.fatSat * setCoef,
+			frcarbs: el.carbs * setCoef,
+			frsugar: el.sugar * setCoef,
+			frfiber: el.fiber * setCoef,
+			frprotein: el.protein * setCoef,
+			frsalt: el.salt * setCoef,
+			frcalories: el.calories * setCoef,
+			frjoules: el.joules * setCoef,
+			weight: parseInt(formProps[element]),
+			coef: setCoef
+		};
+	}
+
+	function sumOfValue(previousValue, currentValue, currentIndex, array) {
+		return previousValue + array.prop;
+	}
+
+	function formatWeight(weight) {
+		return Math.round(weight * 100) / 100;
+	}
+
 	const handleForm = (e) => {
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		const a = parseInt(formProps['Kakava']) / totalWeight;
-		console.log(formProps, totalWeight, a);
+		let b = totalWeight;
+		// const a = Object.keys(formProps).map((element, index, array) => {
+		// 	return editIngr.map((el, ind, arr) => {
+		// 		if (el.name === element) {
+		// 			let setCoef = Math.round((parseInt(formProps[element]) / totalWeight) * 1000) / 1000;
+		// 			let ingrCoef = {
+		// 				name: el.name,
+		// 				id: el.id,
+		// 				frfat: el.fat * setCoef,
+		// 				frfatSat: el.fatSat * setCoef,
+		// 				frcarbs: el.carbs * setCoef,
+		// 				frsugar: el.sugar * setCoef,
+		// 				frfiber: el.fiber * setCoef,
+		// 				frprotein: el.protein * setCoef,
+		// 				frsalt: el.salt * setCoef,
+		// 				frcalories: el.calories * setCoef,
+		// 				frjoules: el.joules * setCoef,
+		// 				weight: parseInt(formProps[element]),
+		// 				coef: setCoef
+		// 			};
+
+		// 			return ingrCoef;
+		// 		} else {
+		// 			return false;
+		// 		}
+		// 	});
+		// });
+
+		const arrayOfArrays = Object.keys(formProps).map((element, index, array) => {
+			return editIngr.reduce(function (arr, el) {
+				if (el.name === element) {
+					arr.push(calcIngredientFraction(el, formProps, element));
+				}
+				return arr;
+			}, []);
+		});
+
+		const ingredientFractionArray = arrayOfArrays.map((el) => {
+			return el[0];
+		});
+
+		console.log(formProps, totalWeight, ingredientFractionArray);
+
+		const valKeys = Object.keys(ingredientFractionArray[0]).filter((el) => el.startsWith('fr'));
+
+		nutritionalValueList = valKeys.reduce((result, field, index) => {
+			const sum = ingredientFractionArray.reduce((acc, object) => {
+				return acc + object[field];
+			}, 0);
+			result[valKeys[index]] = formatWeight(sum);
+			return result;
+		}, {});
+		console.log(nutritionalValueList);
 	};
 </script>
 
@@ -93,6 +174,21 @@
 	<hr />
 	<article>
 		<p>Bendras svoris: {totalWeight}</p>
-		<h3>100g energetine verte:</h3>
+		<p>Kakavos kiekis: {totalCacao} %</p>
 	</article>
+{/if}
+
+{#if nutritionalValueList.frfat}
+	<div class="flex flex-col w-60">
+		<h3>100g energetine verte:</h3>
+		<p>Riebalai: <span>{nutritionalValueList.frfat}</span></p>
+		<p>Iš jų sočiųjų riebalų: <span>{nutritionalValueList.frfatSat}</span></p>
+		<p>Angliavandeniai: <span>{nutritionalValueList.frcarbs}</span></p>
+		<p>Iš jų cukrų <span>{nutritionalValueList.frsugar}</span></p>
+		<p>Skaildulos: <span>{nutritionalValueList.frfiber}</span></p>
+		<p>Baltymai: <span>{nutritionalValueList.frprotein}</span></p>
+		<p>Druska: <span>{nutritionalValueList.frsalt}</span></p>
+		<p>Kcal: <span>{nutritionalValueList.frcalories}</span></p>
+		<p>Kj: <span>{nutritionalValueList.frjoules}</span></p>
+	</div>
 {/if}
--- src/lib/nutritionalValueDatabase.js
+++ src/lib/nutritionalValueDatabase.js
@@ -48,17 +48,31 @@ export const values = [
     {        
         name: "Pieno milteliai",
         id:4,
-        fat: 0,
-        fatSat: 0,
-        carbs: 99,
-        sugar:99,
+        fat: 26.05,
+        fatSat: 15.23,
+        carbs: 38,
+        sugar:38,
         fiber: 0,
-        protein: 0,
-        salt: 0,
-        calories: 396,
-        joules: 1683,
+        protein: 24.5,
+        salt: 1.01,
+        calories: 482,
+        joules: 2016,
         weight: 0,
         coef: 0
     },
 
 ]
+
+export class Ingredient{    
+    constructor(data){
+        this.data = data
+    }
+    setCoef(totalWeight){
+        return this.data.coef = Math.round(this.weight / totalWeight *1000) /1000
+    }
+
+    setWeight(weight){
+        return this.weight = parseInt(weight)
+    }
+    
+}
